852e9e2b4975dea77f7eb883f63be3f93ca2c003
lll <lone.un@gmail.com>
2017-12-20 16:59:54 +0800

    set affinity
    Signed-off-by: lll <lone.un@gmail.com>

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 # coding: UTF-8
+import os
 import sys
+import affinity
 from config.url import urls
 sys.path.insert(0,'./lib/webpy')
 import web
@@ -7,5 +9,9 @@ import web
 app = web.application(urls, globals())
 
 if __name__ == "__main__":
+        pid = os.getpid()
+        print affinity.get_process_affinity_mask(pid)
+        affinity.set_process_affinity_mask(pid, 2)
+        print affinity.get_process_affinity_mask(pid)
         app.run()
 
